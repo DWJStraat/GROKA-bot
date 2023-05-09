22abe817adc8d3f06bd50c3c5ed167d19ca9fefe
Dstra <64135655+DWJStraat@users.noreply.github.com>
2023-05-09 20:31:43 +0200

    Added start.py, which, if the bot is ran through this system, will launch the bot whenever it crashes

---
+++ start.py
@@ -0,0 +1,9 @@
+import os
+import time
+print('starting...')
+while True:
+    os.system("python3 telegram_bot.py")
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    with open("log.txt", "a") as f:
+        f.write(f"/!\{timestamp} | Bot crashed, restarting.../!\\\n")
+    print("Bot crashed, restarting...")
